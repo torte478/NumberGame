63d6fc9c21f05c634e62ab098cb9c500b25fbfda
torte478 <torte478@gmail.com>
2019-12-27 10:38:29 +0300

    init commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entity = new NumberGame.Class1();
+            Console.WriteLine("Hello, world!" + entity.Method1(4));
+        }
+    }
+}
